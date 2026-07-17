b8e43b4a90f71ab8295987c920bdf879d3c54762
1Padun <PadunHZxc@gmail.com>
2026-07-18 00:23:10 +0300

    Configure GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/GitHubPortfolioV2/",
+  
   optimizeDeps: {
     include: ["react", "react-dom/client"],
   },
